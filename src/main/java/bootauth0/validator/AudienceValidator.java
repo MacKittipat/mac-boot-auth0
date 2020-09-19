e03de2f651d6e5a2100a8b9520263b5383dda71f
Mac <mac.kittipat@gmail.com>
2020-09-19 15:26:52 +0700

    Log JWT

--- src/main/java/bootauth0/validator/AudienceValidator.java
+++ src/main/java/bootauth0/validator/AudienceValidator.java
@@ -1,10 +1,12 @@
 package bootauth0.validator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+@Slf4j
 public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
 
     private final String audience;
@@ -15,7 +17,7 @@ public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
 
     public OAuth2TokenValidatorResult validate(Jwt jwt) {
         OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
-
+        log.info("jwt = {}", jwt.getClaims().toString());
         if (jwt.getAudience().contains(audience)) {
             return OAuth2TokenValidatorResult.success();
         }
